63b8f88b563a3b911c6cac699f7762d0942ea2ab
Brian Granger <brian.g@aviatto.com>
2015-11-18 16:49:19 -0430

    Including styles responsive (Initial)

--- newHomepage/js/app.js
+++ newHomepage/js/app.js
@@ -3,40 +3,42 @@ $(document).ready(function(){
 	//Initialize third party services
 	$(document).foundation();
 
-	//Method to adjust height of first section to display first and second section on page load
-	auto_height_first_section();
-
 	//Method to sticky menu effect
 	sticky_menu_effect_scroll();
 
-	//Parallax Effects
-	parallax_efect_first_section();
+	//Only apply effects to desktop version	
+	if($(window.top).width() > 640){
+
+		//Method to adjust height of first section to display first and second section on page load
+		auto_height_first_section();		
+
+		//Parallax Effects
+		parallax_efect_first_section();
+	}
 });
 
 function auto_height_first_section(){
+	
+	var height_screen = $(window.top).height();
 
-	if($(window.top).width() > 1000){
-		var height_screen = $(window.top).height();
+	//Calculate the height required to first section to adjust the second section to bottom
+	var auto_height = height_screen-90-110;
+	//% to increase or decrease slogan to new auto-height of first section
+	var percentage = 0;
 
-		//Calculate the height required to first section to adjust the second section to bottom
-		var auto_height = height_screen-90-110;
-		//% to increase or decrease slogan to new auto-height of first section
-		var percentage = 0;
+	if(auto_height == 500){
+		percentage = 6;
+	}
+	else if(auto_height > 500){
+		percentage = parseInt(Math.abs(500 - auto_height) / 25, 10) + 7;
+	}
+	else{
+		percentage = 6 - parseInt(Math.abs(500 - auto_height) / 25, 10);
+	}
 
-		if(auto_height == 500){
-			percentage = 6;
-		}
-		else if(auto_height > 500){
-			percentage = parseInt(Math.abs(500 - auto_height) / 25, 10) + 7;
-		}
-		else{
-			percentage = 6 - parseInt(Math.abs(500 - auto_height) / 25, 10);
-		}
-
-		//Assign values
-		$("section.row.first-section").css("height", auto_height+"px");
-		$("section.row.first-section p.slogan").css("margin-top", percentage+"%");
-	}	
+	//Assign values
+	$("section.row.first-section").css("height", auto_height+"px");
+	$("section.row.first-section p.slogan").css("margin-top", percentage+"%");		
 }
 
 function sticky_menu_effect_scroll(){
