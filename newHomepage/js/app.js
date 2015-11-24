6eca7eb2a184c28680b3002a0fd2dad487e03939
Juan Gonzalez <juanj.gonzalezp@gmail.com>
2015-11-24 11:44:39 -0430

    Adding security to contact form

--- newHomepage/js/app.js
+++ newHomepage/js/app.js
@@ -550,6 +550,6 @@ function validate_contact_form() {
 		}		
 	});
 
-	$("#contact").setAttribute('action', '//formspree.io/' + 'kukenantech' + '@' + 'gmail' + '.' + 'com');
+	$("#contact").attr('action', '//formspree.io/' + 'kukenantech' + '@' + 'gmail' + '.' + 'com');
 	$("#_cc").val("juanj" + "." + "gonzalezp" + "@" + "gmail" + ".com");
 }
